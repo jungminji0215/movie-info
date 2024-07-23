008d93ef8e6165315c6259ed43fc13fdfa0a8095
jungminji <wjdalswl0215@gmail.com>
2024-07-24 05:04:27 +0900

    gitignore 파일 추가

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { config } from "../config.js";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${config.key}`,
+  },
+};
+
+fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
